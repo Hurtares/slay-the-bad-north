3ce92577ffa6c95dc74aaec6b7ba468a56f87ae2
Bruno Silva <bruno987silva@gmail.com>
2021-02-07 12:40:42 +0000

    Create scripts to manage battleTree

---
+++ Slay the Bad North/Assets/Scripts/BattleNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.CompilerServices;
+
+public class BattleNode
+{
+    public List<BattleNode> previousNode{get;}
+    public int nodeLevel{get;}
+    public bool completed{get;set;}
+    
+    public BattleNode(List<BattleNode> previousNode,int nodeLevel,bool completed){
+        this.previousNode = previousNode;
+        this.nodeLevel = nodeLevel;
+        this.completed = completed;
+    }
+
+
+    public override string ToString(){
+        var numberOfNodes = previousNode == null ? 0 : previousNode.Count;
+        return $" PreviousNodes:{numberOfNodes}, NodeLevel:{nodeLevel}, Completed:{completed}";
+    }
+}
---
+++ Slay the Bad North/Assets/Scripts/BattleTree.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTree : MonoBehaviour
+{
+    [SerializeField] GameObject battleNodePrefab;
+    List<BattleNode> battleNodes;
+    void Start()
+    {
+        battleNodes = new List<BattleNode>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateBattleTree(){
+        battleNodes.Add(new BattleNode( null,0,false ) );
+        battleNodes.Add(new BattleNode(new List<BattleNode>(new BattleNode[]{battleNodes[0]}),1,false));
+        battleNodes.Add(new BattleNode(new List<BattleNode>(new BattleNode[]{battleNodes[1]}),2,false));
+        battleNodes.Add(new BattleNode(new List<BattleNode>(new BattleNode[]{battleNodes[2]}),3,false));
+
+        Debug.Log(battleNodes[0].ToString());
+        Debug.Log(battleNodes[1].ToString());
+        Debug.Log(battleNodes[2].ToString());
+        Debug.Log(battleNodes[3].ToString());
+        battleNodes[0].completed = true;
+        Debug.Log(battleNodes[0].ToString());
+        battleNodes.ForEach( p => PlaceBattleNode(p));
+    }
+
+    void PlaceBattleNode(BattleNode node){
+        var nodeObject = Instantiate(battleNodePrefab,transform);
+        nodeObject.transform.position = new Vector3(nodeObject.transform.position.x,(node.nodeLevel*55)+25,nodeObject.transform.position.z);
+    }
+}
