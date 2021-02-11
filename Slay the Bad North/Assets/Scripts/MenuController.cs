4c66c882d77f9e95bb2d2cccd2cc3f31228692e5
Leandro Sá <leandrosa.esaf@gmail.com>
2021-02-11 23:07:14 +0000

    normalize the menu scale and menu contrller script

---
+++ Slay the Bad North/Assets/Scripts/MenuController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum Race
+{
+    ELF,
+    ORC,
+    HUMAN
+}
+public enum Menu
+{
+    MAIN,
+    OPTIONS,
+    RACE, DECK
+};
+
+public class MenuController : MonoBehaviour
+{
+    Race selectedRace;
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject configMenu;
+    [SerializeField] GameObject raceMenu;
+    [SerializeField] GameObject deckMenu;
+
+    public void startMenu(int iMenu)
+    {
+        Menu menu = (Menu)iMenu;
+        desactiveAll();
+        switch (menu)
+        {
+            case Menu.MAIN:
+                mainMenu.SetActive(true);
+                break;
+            case Menu.OPTIONS:
+                configMenu.SetActive(true);
+                break;
+            case Menu.RACE:
+                raceMenu.SetActive(true);
+                break;
+            case Menu.DECK:
+                deckMenu.SetActive(true);
+                break;
+            default:
+                mainMenu.SetActive(true);
+                break;
+        }
+    }
+
+    public void startGame() {
+
+    }
+
+    public void selectRace(int race)
+    {
+        selectedRace = (Race)race;
+        startMenu(3);
+    }
+
+    private void desactiveAll()
+    {
+        mainMenu.SetActive(false);
+        configMenu.SetActive(false);
+        raceMenu.SetActive(false);
+        deckMenu.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
