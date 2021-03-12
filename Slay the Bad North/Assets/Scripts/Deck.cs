6b2e8d72a4a0774b50eb17bd7d0ae1f56ffa0c29
cseixas17 <kseixas@live.com.pt>
2021-03-12 23:17:46 +0000

    Card class added

---
+++ Slay the Bad North/Assets/Scripts/Deck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public List<Cards> deck = new List<Cards>();
+    public int x;
+    public int deckSize;
+
+    void Start()
+    {
+        x = 0;
+        deckSize = 20;
+        for (int i = 0; i < deckSize; i++)
+        {
+            x = Random.Range(1, 4);
+            deck[i] = CardsDatabase.cardDb[x];
+        }
+
+    }
+
+}
